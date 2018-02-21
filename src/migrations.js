7d8ed784cd6d8ecb5bac1ff56a30d4e59013040f
Ivan Violentov <violentoff@gmail.com>
2018-02-21 18:51:41 +0300

    Update migrations

--- src/migrations.js
+++ src/migrations.js
@@ -7,8 +7,20 @@ const migrateTo070 = (backupData) => {
 
   const { wallets, accounts, salt } = backupData
 
-  const addSaltForWallets = wallet => R.assoc('salt', salt)(wallet)
-  const newWallets = R.map(addSaltForWallets)(accounts)
+  const addSaltAndCustomTypeForWallets = (wallet) => {
+    const { type, isReadOnly } = wallet
+
+    const mnemonicType = isReadOnly ? 'bip32Xpub' : 'mnemonic'
+    const addressType = isReadOnly ? 'address' : 'privateKey'
+    const customType = (type === 'mnemonic') ? mnemonicType : addressType
+
+    return R.compose(
+      R.assoc('salt', salt),
+      R.assoc('customType', customType)
+    )(wallet)
+  }
+
+  const newWallets = R.map(addSaltAndCustomTypeForWallets)(accounts)
 
   return R.compose(
     R.assoc('version', '0.7.0'),
