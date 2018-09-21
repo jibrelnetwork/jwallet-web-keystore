8edfa5bcbb2275ed4e0796390518ff0e7752d2aa
Ivan Violentov <violentoff@gmail.com>
2018-09-21 19:57:54 +0300

    Cleanup flow types

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -3,17 +3,9 @@ declare type Addresses = Array<Address>
 
 declare type Network = string | number
 
-declare type WalletType = 'address' | 'mnemonic'
-declare type WalletCustomType = WalletType | 'bip32Xpub' | 'privateKey'
-
 type EncryptedData = {|
   +data: string,
   +nonce: string,
-|}
-
-declare type WalletEncryptedData = {|
-  +mnemonic: ?EncryptedData,
-  +privateKey: ?EncryptedData,
 |}
 
 declare type ScryptParams = {|
@@ -47,71 +39,6 @@ declare type MnemonicOptionsUser = {|
   passphrase?: string,
   derivationPath?: string,
   paddedMnemonicLength?: number,
-|}
-
-declare type Wallet = {|
-  +passwordOptions: ?PasswordOptions,
-  +mnemonicOptions: ?MnemonicOptions,
-  +encrypted: WalletEncryptedData,
-  +id: string,
-  +name: string,
-  +type: WalletType,
-  +address: ?string,
-  +bip32XPublicKey: ?string,
-  +customType: WalletCustomType,
-  +addressIndex: ?number,
-  +isReadOnly: boolean,
-|}
-
-declare type WalletUpdatedData = {|
-  +passwordOptions?: PasswordOptions,
-  +mnemonicOptions?: MnemonicOptions,
-  +encrypted?: WalletEncryptedData,
-  +name?: string,
-  +bip32XPublicKey?: ?string,
-  +customType?: ?WalletCustomType,
-  +addressIndex?: ?number,
-  +isReadOnly?: ?boolean,
-|}
-
-declare type WalletNewData = {|
-  +scryptParams?: ScryptParams,
-  +data: string,
-  +name?: string,
-  +network?: Network,
-  +passphrase?: string,
-  +derivationPath?: string,
-  +encryptionType?: string,
-  +saltBytesCount?: number,
-  +derivedKeyLength?: number,
-  +paddedMnemonicLength?: number,
-|}
-
-declare type WalletData = {|
-  +passwordOptions: PasswordOptions,
-  +mnemonicOptions: MnemonicOptions,
-  +id: string,
-  +data: string,
-  +name: string,
-|}
-
-declare type WalletDecryptedData = {|
-  +id: string,
-  +name: string,
-  +address: string,
-  +mnemonic: string,
-  +privateKey: string,
-  +type: WalletCustomType,
-  +readOnly: 'yes' | 'no',
-  +bip32XPublicKey: string,
-|}
-
-declare type Wallets = Array<Wallet>
-
-declare type Keystore = {|
-  +wallets: Wallets,
-  +testPasswordData: EncryptedData,
-  +passwordOptions: PasswordOptions,
 |}
 
 declare type PasswordResult = {|
